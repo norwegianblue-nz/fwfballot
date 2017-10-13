21519aac4c317324e309efa8584f2d56604d57a5
roo <roo@norwegian.blue>
2017-10-14 10:42:04 +1300

    Donation page created, (config exported) Ballot management menu and page altered Exposed filters added to payment reconcilliation page

--- web/sites/ballot.fwf.net.nz/modules/custom/draw_hunting_ballot/src/Controller/DrawBallotController.php
+++ web/sites/ballot.fwf.net.nz/modules/custom/draw_hunting_ballot/src/Controller/DrawBallotController.php
@@ -23,14 +23,20 @@ class DrawBallotController extends ControllerBase {
     $options = ['absolute' => TRUE, 'attributes' => ['class' => 'label']];
     $manageballotslink = Link::createFromRoute('Manage Ballots','view.events.page_1',['node' => '0'],$options);
     $manageentrieslink = Link::createFromRoute('Manage Entries','view.allocated_block.page_1',['node' => '0'],$options);
-    $vieworderslink = Link::createFromRoute('View Orders','entity.commerce_order.collection',['node' => '0'],$options);
+    $viewpaymentsUrl = \Drupal\Core\Url::fromUserInput('/admin/commerce/orders/paymentreconciliation-1_1');
+    $viewpaymentsUrl->setOptions($options);
+    $viewpaymentslink = Link::fromTextAndUrl('View Payments',$viewpaymentsUrl);
+    $addballotUrl = \Drupal\Core\Url::fromUserInput('/node/add/event');
+    $addballotUrl->setOptions($options);
+    $createballotlink = Link::fromTextAndUrl('Create New Ballot',$addballotUrl);
     return [
       '#type' => 'markup',
       '#markup' => $this->t('<ul class="admin-list">'
           . '<li>%manageballotslink<div class="description">Choose a ballot to manage (change settings, set up messages and draw the ballot)</div></li>'
           . '<li>%manageentrieslink<div class="description">Display ballot entries and choose from the list entries to manage</div></li>'
-          . '<li>%vieworderslink<div class="description">View orders</div></li>'
-          ,['%manageballotslink' => $manageballotslink->toString(),'%manageentrieslink' => $manageentrieslink->toString(),'%vieworderslink' => $vieworderslink->toString()]
+          . '<li>%viewpaymentslink<div class="description">View ballot payments</div></li>'
+          . '<li>%createballotlink<div class="description">Create ballot entry page for new season</div></li>'
+          ,['%manageballotslink' => $manageballotslink->toString(),'%manageentrieslink' => $manageentrieslink->toString(),'%viewpaymentslink' => $viewpaymentslink->toString(),'%createballotlink' => $createballotlink->toString()]
           ),
     ];
   }
